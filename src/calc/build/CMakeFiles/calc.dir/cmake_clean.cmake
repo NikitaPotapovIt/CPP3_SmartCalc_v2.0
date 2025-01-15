e592cc9011d1503ab4bea7b07777c28738abaa41
nikitapotapov <nikitapotapovit@gmail.com>
2025-01-15 20:02:25 +0300

    Qf no fix

---
+++ src/calc/build/CMakeFiles/calc.dir/cmake_clean.cmake
@@ -0,0 +1,32 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calc_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/calc_autogen.dir/ParseCache.txt"
+  "calc_autogen"
+  "CMakeFiles/calc.dir/Users/nikitapotapov/Documents/Документы_—_MacBook_Pro —_Nikita/School_21/project/CPP3_SmartCalc_v2.0-1/src/smartcalc_controller.cpp.o"
+  "CMakeFiles/calc.dir/Users/nikitapotapov/Documents/Документы_—_MacBook_Pro —_Nikita/School_21/project/CPP3_SmartCalc_v2.0-1/src/smartcalc_controller.cpp.o.d"
+  "CMakeFiles/calc.dir/Users/nikitapotapov/Documents/Документы_—_MacBook_Pro —_Nikita/School_21/project/CPP3_SmartCalc_v2.0-1/src/smartcalc_model.cpp.o"
+  "CMakeFiles/calc.dir/Users/nikitapotapov/Documents/Документы_—_MacBook_Pro —_Nikita/School_21/project/CPP3_SmartCalc_v2.0-1/src/smartcalc_model.cpp.o.d"
+  "CMakeFiles/calc.dir/Users/nikitapotapov/Documents/Документы_—_MacBook_Pro —_Nikita/School_21/project/CPP3_SmartCalc_v2.0-1/src/smartcalc_view.cpp.o"
+  "CMakeFiles/calc.dir/Users/nikitapotapov/Documents/Документы_—_MacBook_Pro —_Nikita/School_21/project/CPP3_SmartCalc_v2.0-1/src/smartcalc_view.cpp.o.d"
+  "CMakeFiles/calc.dir/calc_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/calc.dir/calc_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/calc.dir/credit.cpp.o"
+  "CMakeFiles/calc.dir/credit.cpp.o.d"
+  "CMakeFiles/calc.dir/deposit.cpp.o"
+  "CMakeFiles/calc.dir/deposit.cpp.o.d"
+  "CMakeFiles/calc.dir/main.cpp.o"
+  "CMakeFiles/calc.dir/main.cpp.o.d"
+  "CMakeFiles/calc.dir/mainwindow.cpp.o"
+  "CMakeFiles/calc.dir/mainwindow.cpp.o.d"
+  "CMakeFiles/calc.dir/qcustomplot.cpp.o"
+  "CMakeFiles/calc.dir/qcustomplot.cpp.o.d"
+  "calc.app/Contents/MacOS/calc"
+  "calc.pdb"
+  "calc_autogen/mocs_compilation.cpp"
+  "calc_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

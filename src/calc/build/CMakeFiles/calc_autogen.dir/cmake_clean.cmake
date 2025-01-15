2430a1d5b4c4b4cb04d3c875dfe0ef1826b3d655
nikitapotapov <nikitapotapovit@gmail.com>
2025-01-15 22:39:12 +0300

    fix Qt lib

--- src/calc/build/CMakeFiles/calc_autogen.dir/cmake_clean.cmake
+++ src/calc/build/CMakeFiles/calc_autogen.dir/cmake_clean.cmake
@@ -1,5 +1,7 @@
 file(REMOVE_RECURSE
   "CMakeFiles/calc_autogen"
+  "calc_autogen/include/ui_credit.h"
+  "calc_autogen/include/ui_deposit.h"
   "calc_autogen/include/ui_mainwindow.h"
   "calc_autogen/mocs_compilation.cpp"
   "calc_autogen/timestamp"
